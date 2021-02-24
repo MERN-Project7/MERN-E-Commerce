61dad3d604bbe3cb61b86fc41831c6b6478a6604
laithalenooz <laithalenooz@gmail.com>
2021-02-24 13:03:01 +0200

    Done

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Button, Card, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import Rating from './Rating';
+
+const Product = ({ product, match, history }) => {
+    const [qty, setQty] = useState(1);
+    const [id, setId] = useState({});
+
+
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} style={{ height: 177 }} />
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as='div' style={{ height: 50 }}>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as='div'>
+          <div className='my-3'>
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
+          </div>
+        </Card.Text>
+
+        <Card.Text as='h3'>${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
